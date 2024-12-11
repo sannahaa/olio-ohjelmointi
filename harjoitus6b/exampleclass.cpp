2bfed799b547d4e0fa39a0a363e38ff38d655922
Sanna Haasiomäki <sanna.haasiomaki@gmail.com>
2024-12-11 13:08:33 +0200

    harjoitus 6b

---
+++ harjoitus6b/exampleclass.cpp
@@ -0,0 +1,20 @@
+#include "exampleclass.h"
+
+ExampleClass::ExampleClass(QObject *parent)
+    : QObject{parent}
+{
+    qDebug("Start");
+    connect(this, SIGNAL(readyToSay()), this, SLOT(sayHelloSlot()));
+    qDebug("End");
+}
+
+void ExampleClass::startToWait()
+{
+    QThread::msleep(1000);
+    emit readyToSay();
+}
+
+void ExampleClass::sayHelloSlot()
+{
+    qDebug()<<"Terve";
+}
