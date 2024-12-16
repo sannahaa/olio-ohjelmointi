70b9736ad4eda2c401e002cb775a1f977a78b1e6
Sanna Haasiomäki <sanna.haasiomaki@gmail.com>
2024-12-17 00:11:36 +0200

    Harjoitus 7

---
+++ harjoitus7/mainwindow.cpp
@@ -0,0 +1,34 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+#include <Qstring>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_btnCount_clicked()
+{
+    counter++;
+    QString s = QString::number(counter);
+    ui->labelInfo->setText("Painiketta Count painettu "+s+" kertaa");
+    ui->txtResult->setText(s);
+}
+
+
+void MainWindow::on_btnReset_clicked()
+{
+    counter=0;
+    QString s = QString::number(counter);
+    ui->labelInfo->setText("Painiketta Count painettu "+s+" kertaa");
+    ui->txtResult->setText(s);
+}
+
